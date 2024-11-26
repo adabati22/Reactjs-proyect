15e26f62e033353308700e944d18334d646f8d1a
adabati22 <adara2291@gmail.com>
2024-11-25 23:40:24 -0300

    arreglo2

--- src/assets/components/Navbar/Home.jsx
+++ src/assets/components/Navbar/Home.jsx
@@ -4,8 +4,9 @@ import React, { Component } from 'react'
 export class Home extends Component {
   render() {
     return (
-        <div>
-        
+        <div className='homecito'>
+       <h1>La Cocina de Ori</h1>
+       <img src="https://img.freepik.com/vector-premium/ilustracion-dibujos-animados-lindo-gato-chef_274619-811.jpg" alt="" />
       </div>
     )
   }
